bdbb5f3b4b5851461ad2727d6ae5352f403a3b0d
Everett Kropf <ehkropf@gmail.com>
2016-11-27 15:03:04 +0000

    Add bootstraping file.

---
+++ bin/hcbootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Everett Kropf, 2016
+
+GIT=/usr/bin/git
+REPO=~/Dropbox/Source/hconfig
+THOME=$HOME/tmp/home
+
+# Make sure we're working in the right directory.
+cd $THOME
+
+# Customise git.
+hconfig="$GIT --git-dir=$THOME/.hconfig --work-tree=$THOME"
+
+# Clone repository to home.
+$GIT clone --bare $REPO $THOME/.hconfig
+
+# Try checkout.
+RESULT=/tmp/hcbs.${RANDOM}.txt
+$hconfig checkout 2> $RESULT
+
+if [ $? -ne 0 ]; then
+    echo "Backing up existing files."
+    BDIR=.config-backup
+
+    if [ -d $BDIR ]; then
+        echo "Backup directory already exists. Not overwriting."
+        exit -1
+    fi
+
+    mkdir $BDIR
+
+    files=$(cat $RESULT | egrep "\s+\." | awk {'print $1'})
+    rsync --remove-source-files -aR $files $BDIR
+
+    $hconfig checkout 2> $RESULT
+fi
+cat $RESULT
+
+# Don't show untracked files (the rest of the home directory).
+$hconfig config --local status.showUntrackedFiles no
+
+echo "HConfig bootstrap complete."
